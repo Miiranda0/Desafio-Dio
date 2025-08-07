cf1aa38561be27d715180aee3db54013a2f67df4
Miiranda0 <mirandadavid2021@gmail.com>
2025-08-06 22:17:47 -0300

    atividade pronta

---
+++ index.js
@@ -0,0 +1,56 @@
+// Lê os dados do herói (aqui, usando prompt para exemplo)
+let nome = "Garen"; // Nome do herói
+
+// Lê a quantidade de XP do herói (aqui, usando prompt para exemplo)
+let xp = 9300 ; // XP do herói
+let nivel = ""; 
+
+// Estrutura de decisão para definir o nível do herói
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+} else {
+    nivel = "Desconhecido";
+}
+
+
+console.log ("O herói" + " " + nome + " " + "é do nível" + " " + nivel + " " + "com" + " " + xp + " " + "de XP.");
+
+nome = "David"; // Nome do herói
+xp = 12000; // XP do herói
+nivel = "";
+
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+}  else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+} else {
+    nivel = "Desconhecido";
+}
+
+console.log("O herói " + nome + " é do nível " + nivel + " com " + xp + " de XP.");
